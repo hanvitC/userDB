439e2f5b8925c23dc4c482eecc2287479d42e4e7
Han Choi <121269386+hanvitC@users.noreply.github.com>
2024-10-04 22:13:47 -0400

    Update user.sql

--- user.sql
+++ user.sql
@@ -12,13 +12,3 @@ CREATE TABLE IF NOT EXISTS users (
     last_login       DATETIME DEFAULT NOW(),
     PRIMARY KEY (user_id)
 );
-
-
-CREATE TABLE IF NOT EXISTS active_tokens (
-    token_id         INT NOT NULL,
-    user_id          INT,
-    ttl              INT DEFAULT 3600,  -- Time to live in seconds
-    creation_date    DATETIME DEFAULT NOW(),
-    PRIMARY KEY (token_id),
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
-);
